6e6e657d11109354b292d80b547c429253622696
gena <genagreiner94@gmail.com>
2023-02-17 00:17:04 +0200

    experement1

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ const SITE_PATH = process.env.SITE_PATH || path.join('http://localhost:5000/');
 
 app.use(express.static(__dirname + "/public"));
 app.use(fileUpload({}));
-app.use('/', proxy());
 app.use(cors());
+app.use('/', proxy());
+
 app.use((req, res, next) => {
 res.header("Access-Control-Allow-Origin", "*");
 next();
