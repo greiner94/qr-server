18a2ec010122589c75402dfed5bd1174fa1da835
gena <genagreiner94@gmail.com>
2023-02-17 00:45:11 +0200

    experement1

--- index.js
+++ index.js
@@ -11,15 +11,14 @@ const proxy = require('html2canvas-proxy');
 const PORT = process.env.PORT || 5000;
 const SITE_PATH = process.env.SITE_PATH || path.join('http://localhost:5000/');
 
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept,Authorization,Origin");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+  });
 app.use(express.static(__dirname + "/public"));
-app.use(fileUpload({}));
-app.use(cors());
-app.use('/', proxy());
-
-app.use((req, res, next) => {
-res.header("Access-Control-Allow-Origin", "*");
-next();
-});
 
 const filePath = "qr.json";
 
