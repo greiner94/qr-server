9a215bc506e1a333f5a5f850249101ad470d2b1e
gena <genagreiner94@gmail.com>
2023-02-16 21:59:06 +0200

    experement1

--- index.js
+++ index.js
@@ -13,12 +13,7 @@ const SITE_PATH = process.env.SITE_PATH || path.join('http://localhost:5000/');
   
 app.use(express.static(__dirname + "/public"));
 app.use(fileUpload({}));
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', '*');
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+app.use(cors());
 
 const filePath = "qr.json";
 
